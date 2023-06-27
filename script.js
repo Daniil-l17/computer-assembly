443f0ab1883a31de3acbd3f8bbbc8d4d1e7783d2
Daniil-l17 <129774580+Daniil-l17@users.noreply.github.com>
2023-06-27 19:09:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const button = document.querySelector('.vgg555');
+button.addEventListener('click', () => {
+  button.classList.toggle('clicked');
+});
+
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+  btn.classList.toggle('clicked');
+});
+
+const vf = document.getElementById('vf');
+vf.addEventListener('click', () => {
+  vf.classList.toggle('clicked');
+});
+
